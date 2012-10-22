71286ab660f5d192b4744f6b7d9d0c1b9e7d89ad
Marco Mornati <ilmorna@gmail.com>
2012-10-22 13:59:33 +0200

    Moved from GET Method to POST Added logger with debug information

--- mc-rpc-restserver.rb
+++ mc-rpc-restserver.rb
@@ -21,120 +21,216 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Examples :
-# http://<your box>:4567/mcollective/no-filter/rpcutil/ping/
-# http://<your box>:4567/mcollective/no-filter/package/status/package=bash
+# http://<your box>:4567/mcollective/rpcutil/ping/
+# JSON Available Values:
+#{
+# filters: {
+#   class: [value,value],
+#   identity: [value]
+#   fact: [],
+#   agent: [],
+#   compound: value
+# },
+# parameters: {
+#   name: value,
+#   name, value
+# },
+# schedule: {
+#   schedtype: value,
+#   schedarg: value
+# }
+# limit: {
+#   targets: value,
+#   method: value
+# }
+#}
 #
-# GET /mcollective/class_filter=retail;class_filter=stores/package/status/package=bash
 #
+# POST /mcollective/package/status/
+# JSON Obj in POST:
+# {
+#    "filters": {
+#        "class": ['retail', 'stores']
+#    },
+#    "parameters": {
+#        "package": "bash"
+#    }
+# }
+#
+# POST /mcollective/service/status/
+# JSON Obj:
+# {"parameters": {"service":"sshd"}, "limit": {"targets":"1"}}
+# of with filters
+# {"filters":{"identity":["notebook", "el6"]}, "parameters": {"service":"sshd"}, "limit": {"targets":"1"}}
 # Returns all the answers as a JSON data block
 
 require 'rubygems'
 require 'sinatra'
 require 'mcollective'
 require 'json'
+require 'logger'
+require 'inifile'
 
 include MCollective::RPC
+
+def getkey(section, key)
+    ini=IniFile.load('/etc/sysconfig/kermit-restmco', :comment => '#')
+    params = ini[section]
+    params[key]
+end
+
+#Read Configuration file
+LOG_FILE = getkey('logger', 'LOG_FILE')
+LOG_LEVEL = getkey('logger', 'LOG_LEVEL')
+
+#Create log file if does not exists
+FileUtils.touch LOG_FILE
+FileUtils.chown('nobody', 'nobody', LOG_FILE)
 
 uid = Etc.getpwnam("nobody").uid
 Process::Sys.setuid(uid)
 
+#Create Log file
+logger = Logger.new(STDERR)
+logger = Logger.new(STDOUT)
+logger = Logger.new(LOG_FILE, 'daily')
+case LOG_LEVEL
+    when 'DEBUG'
+        logger.level = Logger::DEBUG
+    when 'INFO'
+        logger.level = Logger::INFO
+    when 'WARN'
+        logger.level = Logger::WARN
+    when 'ERROR'
+        logger.level = Logger::ERROR
+end
+
+logger.debug "Starting Kermit-RestMCO"
 
 def set_filters(mc, params)
-    if params[:filters] && params[:filters] != 'no-filter' then
-        params[:filters].split(';').each do |filter|
-            name,value = $1, $2 if filter =~ /^(.+?)=(.+)$/
-            puts "#{name}: #{value}"
+    if params[:filters] then
+        logger.info "Applying filters"
+        params[:filters].each do |filter_type, filter_values|
+            logger.debug "#{filter_type}: #{filter_values}"
             case name
-            when 'class_filter'
+            when 'class'
                 puts "Applying class_filter"
-                mc.class_filter "/#{value}/"
-            when 'fact_filter'
+                filter_values.each do |value|
+                    mc.class_filter "/#{value}/"
+                end
+            when 'fact'
                 puts "Applying fact_filter"
-                mc.fact_filter "#{value}"
-            when 'agent_filter'
+                filter_values.each do |value|
+                    mc.fact_filter "#{value}"
+                end
+            when 'agent'
                 puts "Applying agent_filter"
-            when 'limit_targets'
-                puts "Applying limit_targets"
-                mc.limit_targets = "#{value}"
-            when 'identity_filter'
+            #when 'limit'
+            #    puts "Applying limit_targets"
+            #    filter_values.each do |value|
+            #        mc.limit_targets = "#{value}"
+            #    end
+            when 'identity'
                 puts "Applying identity_filter"
-                mc.identity_filter "#{id_filter(value)}"
+                filter_values.each do |value|
+                    mc.identity_filter "#{value}"
+                end
+            when 'compound'
+                puts "Applying compound_filter"
+                filter_values.each do |value|
+                    mc.compound "#{value}"
+                end
             end
         end
     end
 end
 
-def id_filter(value)
-   value_list = value.split('_OR_')
-   if value_list.length > 1
-       regex_string = "/"
-       value_list.each_with_index do |o,i|
-           if i != 0 then
-               regex_string << '|'
-           end
-           regex_string << o
-       end
-       regex_string << "/"
-       return "#{regex_string}"
-   end 
-   "#{value}"
-end 
-
-def arg_parse(params)
-    arguments = {}
-    params[:splat].each do |args|
-        args.split(';').each do |arg|
-            arguments[$1.to_sym] = $2 if arg =~ /^(.+?)=(.+)$/
-        end
-    end
-    arguments
+def recursive_symbolize_keys(h)
+  case h
+  when Hash
+    Hash[
+      h.map do |k, v|
+        [ k.respond_to?(:to_sym) ? k.to_sym : k, recursive_symbolize_keys(v) ]
+      end
+    ]
+  when Enumerable
+    h.map { |v| recursive_symbolize_keys(v) }
+  else
+    h
+  end
 end
 
-
 get '/' do
+    logger.debug "Calling / url"
     "Hello Sinatra"
 end
 
-# GET /schedule/in/0s/no-filter/rpcutil/ping/
-# GET /schedule/in/60s/identity_filter=el4/package/status/package=bash
-get '/schedule/:schedtype/:schedarg/:filters/:agent/:action/*' do
-    params[:schedtype] ||='in'
-    params[:schedarg]  ||='0s'
-    jobreq = { :agentname  => params[:agent],
-               :actionname => params[:action],
-               :schedtype  => params[:schedtype],
-               :schedarg   => params[:schedarg] }
+get '/schedstatus/:jobid/:filters' do
+    logger.debug "Calling /schedstatus url"
+    jobreq = { :jobid => params[:jobid] }
     sched = rpcclient("scheduler")
     set_filters(sched, params)
-    arguments = arg_parse(params)
-    arguments.each  { |name,value| puts "#{name}: #{value}" }
-    unless arguments.empty?
-       jobreq[:params] = arguments.keys.join(",")
-       jobreq.merge!(arguments)
-    end
-    JSON.dump(sched.schedule(jobreq).map{|r| r.results})
-end
-
-get '/schedstatus/:jobid/:filters' do
-   jobreq = { :jobid => params[:jobid] }
-   sched = rpcclient("scheduler")
-   set_filters(sched, params)
-   JSON.dump(sched.query(jobreq).map{|r| r.results})
+    json_response = JSON.dump(sched.query(jobreq).map{|r| r.results})
+    logger.info "Command schedstatus #{params[:jobid]} executed on filters #{params[:filters]}"
+    logger.debug "Response received: #{json_response}"
+    json_response
 end
 
 get '/schedoutput/:jobid/:filters' do
-   jobreq = { :jobid => params[:jobid], :output => 'yes' }
-   sched = rpcclient("scheduler")
-   set_filters(sched, params)
-   JSON.dump(sched.query(jobreq).map{|r| r.results})
+    logger.debug "Calling /schedoutput url"
+    jobreq = { :jobid => params[:jobid], :output => 'yes' }
+    sched = rpcclient("scheduler")
+    set_filters(sched, params)
+    json_response = JSON.dump(sched.query(jobreq).map{|r| r.results})
+    logger.info "Command scheoutput #{params[:jobid]} executed on filters: #{params[:filters]}"
+    logger.debug "Response received: #{json_response}"
+    json_response
 end
 
-get '/mcollective/:filters/:agent/:action/*' do
-    mc = rpcclient(params[:agent])
-    mc.discover
-    set_filters(mc, params)
-    arguments = arg_parse(params)
-    arguments.each  { |name,value| puts "#{name}: #{value}" }
-    JSON.dump(mc.send(params[:action], arguments).map{|r| r.results})
+post '/mcollective/:agent/:action/' do
+    logger.debug "Calling /mcollective url"
+    data = JSON.parse(request.body.read)
+    data = recursive_symbolize_keys(data)
+    logger.debug "JSON Data: #{data}"
+    if data[:schedule] then
+        logger.info "Executing with backend scheduler"
+        scheduler_data=data[:schedule]
+        scheduler_data[:schedtype] ||='in'
+        scheduler_data[:schedarg]  ||='0s'
+        jobreq = { :agentname  => params[:agent],
+                   :actionname => params[:action],
+                   :schedtype  => scheduler_data[:schedtype],
+                   :schedarg   => scheduler_data[:schedarg] }
+        sched = rpcclient("scheduler")
+        set_filters(sched, params)
+        unless arguments.empty?
+            jobreq[:params] = data[:parameters].keys.join(",")
+            jobreq.merge!(data[:parameters])
+        end
+        json_response = JSON.dump(sched.schedule(jobreq).map{|r| r.results})
+        logger.info "Command Agent: #{params[:agent]} Action: #{params[:action]} executed"
+        logger.debug "Response received: #{json_response}"
+        json_response
+    else
+        mc = rpcclient(params[:agent])
+        mc.discover
+        set_filters(mc, params)
+        if data[:parameters]
+            data[:parameters].each  { |name,value| puts "#{name}: #{value}" }
+        end
+        if data[:limit]
+            limits = data[:limit]
+            if limits[:target]
+                mc.limit_targets = "#{limits[:targets]}"
+            end
+            if limits[:method]
+                mc.limit_method = "#{limits[:method]}"
+            end
+        end
+        json_response = JSON.dump(mc.send(params[:action], data[:parameters]).map{|r| r.results})
+        logger.info "Command Agent: #{params[:agent]} Action: #{params[:action]} executed"
+        logger.debug "Response received: #{json_response}"
+        json_response
+    end
 end
 
